da7a3dac8373acc866af8bb3e2fbc64a17e9247b
896660689 <896660689@qq.com>
2022-04-28 18:41:09 +0800

    Update shadowsocks.sh

--- trunk/user/shadowsocks/scripts/shadowsocks.sh
+++ trunk/user/shadowsocks/scripts/shadowsocks.sh
@@ -318,7 +318,6 @@ ipt_ss_del()
     for setname in $(ipset -n list | grep "gfwlist"); do
         ipset destroy "$setname" 2>/dev/null
     done
-    sleep 1 && restart_dhcpd
 }
 
 func_sshome_file(){
@@ -398,7 +397,8 @@ func_stop(){
     func_ss_Close && \
     ipt_ss_del && \
     func_ss_down &
-    sleep 2 && logger -t "[ShadowsocksR]" "已停止运行!"
+    sleep 2 && restart_dhcpd&& \
+    logger -t "[ShadowsocksR]" "已停止运行!"
 }
 
 case "$1" in
