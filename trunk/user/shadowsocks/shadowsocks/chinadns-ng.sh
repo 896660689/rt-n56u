81a4f61b0b6b01f77fecaf68a64790364dda102a
896660689 <zhangxuan000@live.com>
2023-03-06 09:45:34 +0800

    Update chinadns-ng.sh

--- trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
+++ trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
@@ -24,6 +24,16 @@ func_del_rule(){
 }
 
 func_del_ipt(){
+ipt="iptables -t nat"
+$ipt -D PREROUTING -j CNNG_OUT
+$ipt -D OUTPUT -j CNNG_PRE
+$ipt -D CNNG_PRE -d $v2_address -j RETURN
+$ipt -D CNNG_PRE -m set --match-set gateway dst -j RETURN
+$ipt -D CNNG_PRE -m set --match-set chnroute dst -j RETURN
+$ipt -D CNNG_OUT -m set --match-set chnroute dst -j RETURN
+$ipt -D CNNG_OUT -p udp -d 127.0.0.1 --dport 53 -j REDIRECT --to-ports 65353
+$ipt -D CNNG_OUT -p tcp -j CNNG_PRE
+$ipt -D CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
 iptables-save -c | grep -v gateway | iptables-restore -c
 for setname in $(ipset -n list | grep "gateway"); do
     ipset destroy "$setname" 2>/dev/null
