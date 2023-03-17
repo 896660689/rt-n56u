0b9a78a55e6bf5bcea272b4a82e4bd24102a132e
896660689 <896660689@qq.com>
2023-03-17 21:20:27 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -589,7 +589,7 @@ struct nvram_pair router_defaults[] = {
 	{ "zero_staticnum_x", "0" },
 #endif
 
-#if defined(SmartDns)
+#if defined(APP_SMARTDNS)
 	/*SmartDns*/
 	{ "sdns_enable", "0" },
 	{ "snds_name", "smartdns" },
@@ -598,16 +598,24 @@ struct nvram_pair router_defaults[] = {
 	{ "sdns_ipv6_server", "0" },
 	{ "snds_ip_change", "0" },
 	{ "snds_ip_change_time", "30" },
-	{ "sdns_ipv6", "0" },
-	{ "sdns_www", "0" },
-	{ "sdns_exp", "0" },
-	{ "snds_redirect", "0" },
-	{ "snds_cache", "0" },
-	{ "sdns_ttl", "300" },
-	{ "sdns_ttl_min", "60" },
+	{ "sdns_ipv6", "1" },
+	{ "sdns_www", "1" },
+	{ "sdns_exp", "1" },
+	{ "sdns_exp_ttl", "0" },
+	{ "sdns_exp_ttl_max", "10" },
+	{ "sdns_cache_persist", "1" },
+	{ "snds_redirect", "1" },
+	{ "snds_cache", "10240" },
+	{ "sdns_ttl", "21600" },
+	{ "sdns_ttl_min", "1800" },
 	{ "sdns_ttl_max", "86400" },
 	{ "sdns_coredump", "0" },
 	{ "sdnss_staticnum_x", "0" },
+	{ "sdns_speed", "0" },
+	{ "sdns_address", "0" },
+	{ "sdns_as", "0" },
+	{ "sdns_ipset", "0" },
+	{ "sdns_ns", "0" },
 	{ "sdnse_enable", "0" },
 	{ "sdnse_port", "7053" },
 	{ "sdnse_tcp", "0" },
@@ -621,6 +629,9 @@ struct nvram_pair router_defaults[] = {
 	{ "sdnse_cache", "0" },
 	{ "ss_white", "0" },
 	{ "ss_black", "0" },
+	{ "sdns_change", "2" },
+	{ "sdns_change1", "2" },
+	{ "sdns_change2", "2" },
 #endif
 
 	/* DHCP server parameters */
