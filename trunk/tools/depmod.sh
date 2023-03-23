015d7e0e99434a7b2f3bbe4fe9657fa08ff61638
896660689 <896660689@qq.com>
2023-03-23 23:13:49 +0800

    Update depmod.sh

--- trunk/tools/depmod.sh
+++ trunk/tools/depmod.sh
@@ -24,7 +24,7 @@ if [ ! -x "$depmod_bin" ]; then
 fi
 
 if [ "$CONFIG_FIRMWARE_INCLUDE_IPSET" = "y" ] ; then
-	ipset_dir="${ROOTDIR}/user/ipset/ipset-6.x/kernel/net/netfilter"
+	ipset_dir="${ROOTDIR}/user/ipset/ipset-7.1/kernel/net/netfilter"
 	mkdir -p "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset"
 	cp -f "$ipset_dir/xt_set.ko" "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter"
 	cp -f "$ipset_dir/ipset/"*.ko "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset"
@@ -60,8 +60,8 @@ rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.softdep"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.symbols"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.symbols.bin"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.builtin"
-rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.builtin.alias.bin"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.builtin.bin"
+rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.builtin.alias.bin"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.order"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/build"
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/source"
