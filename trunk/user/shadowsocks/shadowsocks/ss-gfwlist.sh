93cce466983ef7139724d6ed462fca126dfe3a17
896660689 <896660689@qq.com>
2023-07-27 03:45:34 +0800

    Update ss-gfwlist.sh

--- trunk/user/shadowsocks/shadowsocks/ss-gfwlist.sh
+++ trunk/user/shadowsocks/shadowsocks/ss-gfwlist.sh
@@ -131,6 +131,7 @@ include_ac_rules() {
 	-A gfwlist -d 203.0.113.0/24 -j RETURN
  	-A gfwlist -d 224.0.0.0/4 -j RETURN
 	-A gfwlist -d 240.0.0.0/4 -j RETURN
+ 	-A gfwlist -d 255.255.255.255/32 -j RETURN
  
 	-A gfwlist -m set --match-set chnroute dst -j RETURN
 	COMMIT
