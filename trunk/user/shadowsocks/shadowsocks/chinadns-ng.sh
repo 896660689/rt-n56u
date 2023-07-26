d1be8bb4e75f9c4766a311347e715cd195afd7be
896660689 <896660689@qq.com>
2023-07-26 18:53:21 +0800

    Update chinadns-ng.sh

--- trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
+++ trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
@@ -53,12 +53,21 @@ func_del_ipt(){
     $ipt -D CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
     $ipt -D CNNG_OUT -d 0.0.0.0/8 -j RETURN
     $ipt -D CNNG_OUT -d 10.0.0.0/8 -j RETURN
+    $ipt -D CNNG_OUT -d 100.64.0.0/10 -j RETURN
     $ipt -D CNNG_OUT -d 127.0.0.0/8 -j RETURN
     $ipt -D CNNG_OUT -d 169.254.0.0/16 -j RETURN
     $ipt -D CNNG_OUT -d 172.16.0.0/12 -j RETURN
+    $ipt -D CNNG_OUT -d 192.0.0.0/24 -j RETURN
+    $ipt -D CNNG_OUT -d 192.0.2.0/24 -j RETURN
+    $ipt -D CNNG_OUT -d 192.88.99.0/24 -j RETURN
     $ipt -D CNNG_OUT -d 192.168.0.0/16 -j RETURN
+    $ipt -D CNNG_OUT -d 198.18.0.0/15 -j RETURN
+    $ipt -D CNNG_OUT -d 198.51.100.0/24 -j RETURN
+    $ipt -D CNNG_OUT -d 169.254.0.0/16 -j RETURN
+    $ipt -D CNNG_OUT -d 203.0.113.0/24 -j RETURN
     $ipt -D CNNG_OUT -d 224.0.0.0/4 -j RETURN
     $ipt -D CNNG_OUT -d 240.0.0.0/4 -j RETURN
+    $ipt -D CNNG_OUT -d 255.255.255.255/32 -j RETURN
     #$ipt -D CNNG_PRE -m set --match-set gfwlist dst -j CNNG_OUT
     #$ipt -D CNNG_PRE -j CNNG_OUT
     $ipt -D CNNG_OUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $ss_local_port
@@ -97,34 +106,34 @@ EOF
 }
 
 gen_wa_fw_ip() {
-	cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
-		216.58.200.238
-		217.160.0.201
-		172.217.160.78
-		172.217.24.3
-		172.217.160.110
-		104.244.42.129
-		104.18.9.230
-		104.18.27.103
-		67.228.126.62
-		176.9.146.200
-		46.4.7.165
-		195.201.59.244
-		136.243.22.80
-		78.46.27.186
-		85.10.210.166
-		91.108.12.0/22
-		91.108.4.0/22
-		91.108.8.0/22
-		91.108.16.0/22
-		91.108.20.0/22
-		91.108.36.0/23
-		91.108.38.0/23
-		91.108.56.0/22
-		149.154.160.0/20
-		149.154.164.0/22
-		204.246.176.0/20
-		149.154.172.0/22
+    cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
+	216.58.200.238
+	217.160.0.201
+	172.217.160.78
+	172.217.24.3
+	172.217.160.110
+	104.244.42.129
+	104.18.9.230
+	104.18.27.103
+	67.228.126.62
+	176.9.146.200
+	46.4.7.165
+	195.201.59.244
+	136.243.22.80
+	78.46.27.186
+	85.10.210.166
+	91.108.12.0/22
+	91.108.4.0/22
+	91.108.8.0/22
+	91.108.16.0/22
+	91.108.20.0/22
+	91.108.36.0/23
+	91.108.38.0/23
+	91.108.56.0/22
+	149.154.160.0/20
+	149.154.164.0/22
+	204.246.176.0/20
+	149.154.172.0/22
 EOF
 }
 
@@ -182,85 +191,94 @@ EOF
 }
 
 func_gmlan(){
-ipset -! restore <<-EOF
-create gateway hash:net hashsize 64
-$(gen_lan_ip | sed -e "s/^/add gateway /")
+    ipset -! restore <<-EOF
+        create gateway hash:net hashsize 64
+        $(gen_lan_ip | sed -e "s/^/add gateway /")
 EOF
 }
 
 gen_lan_ip(){
-cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
-0.0.0.0/8
-10.0.0.0/8
-100.64.0.0/10
-127.0.0.0/8
-169.254.0.0/16
-172.16.0.0/12
-192.0.0.0/24
-192.0.2.0/24
-192.88.99.0/24
-192.168.0.0/16
-198.18.0.0/15
-198.51.100.0/24
-203.0.113.0/24
-224.0.0.0/4
-240.0.0.0/4
-255.255.255.255/32
+    cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
+        0.0.0.0/8
+        10.0.0.0/8
+        100.64.0.0/10
+        127.0.0.0/8
+        169.254.0.0/16
+        172.16.0.0/12
+        192.0.0.0/24
+        192.0.2.0/24
+        192.88.99.0/24
+        192.168.0.0/16
+        198.18.0.0/15
+        198.51.100.0/24
+        203.0.113.0/24
+        224.0.0.0/4
+        240.0.0.0/4
+        255.255.255.255/32
 EOF
 }
 
 flush_ipt_file(){
-FWI="/tmp/shadowsocks_iptables.save"
-[ -n "$FWI" ] && echo '# firewall include file' >$FWI && \
-chmod +x $FWI
-return 0
+    FWI="/tmp/shadowsocks_iptables.save"
+    [ -n "$FWI" ] && echo '# firewall include file' >$FWI && \
+    chmod +x $FWI
+    return 0
 }
 
 func_ipt_n(){
-if grep -q "vmess" "$STORAGE_V2SH"
-then
-    V2RUL=/tmp/V2mi.txt
-    v2_address=$(sed -n "2p" $V2RUL | cut -f 2 -d ":")
-else
-    v2_address=$(cat $STORAGE_V2SH | grep "address" | awk -F '[:/]' '{print $2}')
-fi
-sleep 2
-ipt="iptables -t nat"
+    if grep -q "vmess" "$STORAGE_V2SH"
+    then
+        V2RUL=/tmp/V2mi.txt
+        v2_address=$(sed -n "2p" $V2RUL | cut -f 2 -d ":")
+    else
+        v2_address=$(cat $STORAGE_V2SH | grep "address" | awk -F '[:/]' '{print $2}')
+    fi
+    sleep 2
+    ipt="iptables -t nat"
 
-$ipt -N CNNG_OUT
-$ipt -N CNNG_PRE
+    $ipt -N CNNG_OUT
+    $ipt -N CNNG_PRE
 
-$ipt -A PREROUTING -j CNNG_OUT
-$ipt -A OUTPUT -j CNNG_PRE
-$ipt -A CNNG_PRE -d $v2_address -p tcp -m tcp ! --dport 53 -j RETURN
-$ipt -A CNNG_PRE -m set --match-set gateway dst -j RETURN
-#$ipt -A CNNG_PRE -m set --match-set chnroute dst -j RETURN
-$ipt -A CNNG_OUT -m set --match-set chnroute dst -j RETURN
-$ipt -A CNNG_OUT -p udp -d 127.0.0.1 --dport 53 -j REDIRECT --to-ports 65353
+    $ipt -A PREROUTING -j CNNG_OUT
+    $ipt -A OUTPUT -j CNNG_PRE
+    $ipt -A CNNG_PRE -d $v2_address -p tcp -m tcp ! --dport 53 -j RETURN
+    $ipt -A CNNG_PRE -m set --match-set gateway dst -j RETURN
+    #$ipt -A CNNG_PRE -m set --match-set chnroute dst -j RETURN
+    $ipt -A CNNG_OUT -m set --match-set chnroute dst -j RETURN
+    $ipt -A CNNG_OUT -p udp -d 127.0.0.1 --dport 53 -j REDIRECT --to-ports 65353
 
-$ipt -A CNNG_OUT -p tcp -j CNNG_PRE
-$ipt -A CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
+    $ipt -A CNNG_OUT -p tcp -j CNNG_PRE
+    $ipt -A CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
 
-$ipt -A CNNG_OUT -d 0.0.0.0/8 -j RETURN
-$ipt -A CNNG_OUT -d 10.0.0.0/8 -j RETURN
-$ipt -A CNNG_OUT -d 127.0.0.0/8 -j RETURN
-$ipt -A CNNG_OUT -d 169.254.0.0/16 -j RETURN
-$ipt -A CNNG_OUT -d 172.16.0.0/12 -j RETURN
-$ipt -A CNNG_OUT -d 192.168.0.0/16 -j RETURN
-$ipt -A CNNG_OUT -d 224.0.0.0/4 -j RETURN
-$ipt -A CNNG_OUT -d 240.0.0.0/4 -j RETURN
-#$ipt -A CNNG_PRE -j CNNG_OUT
-#$ipt -A CNNG_PRE -m set --match-set gfwlist dst -j CNNG_OUT
-$ipt -A CNNG_OUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $ss_local_port
+    $ipt -A CNNG_OUT -d 0.0.0.0/8 -j RETURN
+    $ipt -A CNNG_OUT -d 10.0.0.0/8 -j RETURN
+    $ipt -A CNNG_OUT -d 100.64.0.0/10 -j RETURN
+    $ipt -A CNNG_OUT -d 127.0.0.0/8 -j RETURN
+    $ipt -A CNNG_OUT -d 169.254.0.0/16 -j RETURN
+    $ipt -A CNNG_OUT -d 172.16.0.0/12 -j RETURN
+    $ipt -A CNNG_OUT -d 192.0.0.0/24 -j RETURN
+    $ipt -A CNNG_OUT -d 192.0.2.0/24 -j RETURN
+    $ipt -A CNNG_OUT -d 192.88.99.0/24 -j RETURN
+    $ipt -A CNNG_OUT -d 192.168.0.0/16 -j RETURN
+    $ipt -A CNNG_OUT -d 198.18.0.0/15 -j RETURN
+    $ipt -A CNNG_OUT -d 198.51.100.0/24 -j RETURN
+    $ipt -A CNNG_OUT -d 169.254.0.0/16 -j RETURN
+    $ipt -A CNNG_OUT -d 203.0.113.0/24 -j RETURN
+    $ipt -A CNNG_OUT -d 224.0.0.0/4 -j RETURN
+    $ipt -A CNNG_OUT -d 240.0.0.0/4 -j RETURN
+    $ipt -A CNNG_OUT -d 255.255.255.255/32 -j RETURN
+    #$ipt -A CNNG_PRE -j CNNG_OUT
+    #$ipt -A CNNG_PRE -m set --match-set gfwlist dst -j CNNG_OUT
+    $ipt -A CNNG_OUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $ss_local_port
 
-cat <<-CAT >>$FWI
-iptables-save -c | grep -v CNNG_ | iptables-restore -c
-iptables-restore -n <<-EOF
-$(iptables-save | grep -E "CNNG_|^\*|^COMMIT" |\
-sed -e "s/^-A \(OUTPUT\|PREROUTING\)/-I \1 1/")
+    cat <<-CAT >>$FWI
+    iptables-save -c | grep -v CNNG_ | iptables-restore -c
+    iptables-restore -n <<-EOF
+        $(iptables-save | grep -E "CNNG_|^\*|^COMMIT" |\
+        sed -e "s/^-A \(OUTPUT\|PREROUTING\)/-I \1 1/")
 EOF
-CAT
-return 0
+    CAT
+    return 0
 }
 
 func_start(){
@@ -297,3 +315,4 @@ stop)
     exit 1
     ;;
 esac
+
