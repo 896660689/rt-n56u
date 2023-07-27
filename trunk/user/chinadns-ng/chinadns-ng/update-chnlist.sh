b35a07e98f324ef8d29077fe65431ee642a2ef2f
896660689 <zhangxuan000@live.com>
2023-07-27 22:37:23 +0800

    Add files via upload

--- trunk/user/chinadns-ng/chinadns-ng/update-chnlist.sh
+++ trunk/user/chinadns-ng/chinadns-ng/update-chnlist.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-url='https://raw.github.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
-data=$(curl -4sSkL "$url") || { echo "download failed, exit-code: $?"; exit 1; }
+set -o errexit
+set -o pipefail
+
+# exit if curl failed
+url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
+
 echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
