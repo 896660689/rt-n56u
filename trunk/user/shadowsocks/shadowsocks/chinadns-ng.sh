2507ad8ae6f067e15f27e6b7b5902d443512515c
896660689 <896660689@qq.com>
2024-07-21 15:47:55 +0800

    Update chinadns-ng.sh

--- trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
+++ trunk/user/shadowsocks/shadowsocks/chinadns-ng.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Compile:by-lanse	2024-07-21
+# Compile:by-lanse	2023-07-26
 
 modprobe xt_set
 modprobe ip_set_hash_ip
@@ -42,31 +42,128 @@ func_del_rule(){
 }
 
 func_del_ipt(){
-iptables-save -c | grep -v CNNG_ | iptables-restore -c
-for setname in $(ipset -n list | grep "gateway"); do
-    ipset destroy "$setname" 2>/dev/null
-done
+    ipt="iptables -t nat"
+    ip rule del fwmark 0x01/0x01 table 100 2>/dev/null
+    ip route del local 0.0.0.0/0 dev lo table 100 2>/dev/null
+    $ipt -D CNNG_PRE -d $v2_address -p tcp -m tcp ! --dport 53 -j RETURN
+    $ipt -D CNNG_PRE -m set --match-set gateway dst -j RETURN
+    $ipt -D CNNG_OUT -m set --match-set chnroute dst -j RETURN
+    $ipt -D CNNG_OUT -p udp -d $SS_SERVER_LINK --dport 53 -j REDIRECT --to-ports 65353
+    $ipt -D CNNG_OUT -p tcp -j CNNG_PRE
+    $ipt -D CNNG_PRE -p tcp -j RETURN -m mark --mark 0xff
+    $ipt -D CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
+    $ipt -D CNNG_OUT -d 0.0.0.0/8 -j RETURN
+    $ipt -D CNNG_OUT -d 10.0.0.0/8 -j RETURN
+    $ipt -D CNNG_OUT -d 127.0.0.0/8 -j RETURN
+    $ipt -D CNNG_OUT -d 172.16.0.0/12 -j RETURN
+    $ipt -D CNNG_OUT -d 192.168.0.0/16 -j RETURN
+    $ipt -D CNNG_OUT -d 240.0.0.0/4 -j RETURN
+    
+    $ipt -D CNNG_OUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $ss_local_port
+    $ipt -F CNNG_OUT
+    $ipt -X CNNG_OUT
+    $ipt -F CNNG_PRE
+    $ipt -X CNNG_PRE
+
+    iptables-save -c | grep -v gateway | iptables-restore -c
+    for setname in $(ipset -n list | grep "gateway"); do
+        ipset destroy "$setname" 2>/dev/null
+    done
+    $ipt -D PREROUTING -i br0 -p tcp -j CNNG_OUT
+    $ipt -D OUTPUT -p tcp -j CNNG_PRE
 }
 
-func_cdn_file(){
-    logger -t "[CHINADNS-NG]" "下载 [cdn] 域名文件..."
-    curl -k -s -o /tmp/cdn.txt --connect-timeout 10 --retry 3 https://gitee.com/bkye/rules/raw/master/cdn.txt
+ipset_init() {
+    ipset -! restore <<-EOF || return 1
+        create gfwlist hash:net hashsize 64
+        $(gen_wa_fw_ip | sed -e "s/^/add gfwlist /")
+EOF
+    return 0
 }
 
-func_cnng_file(){
-    /usr/bin/chinadns-ng -b 0.0.0.0 -l 65353 -c $wan_dns,114.114.114.114 -t $SS_SERVER_LINK#$ss_tunnel_local_port -4 chnroute >/dev/null 2>&1 &
-    if grep -q "no-resolv" "$DNSMASQ_RURE"
+gen_wa_fw_ip() {
+    cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
+216.58.200.238
+217.160.0.201
+172.217.160.78
+172.217.24.3
+172.217.160.110
+104.244.42.129
+104.18.9.230
+104.18.27.103
+67.228.126.62
+176.9.146.200
+46.4.7.165
+195.201.59.244
+136.243.22.80
+78.46.27.186
+85.10.210.166
+91.108.12.0/22
+91.108.4.0/22
+91.108.8.0/22
+91.108.16.0/22
+91.108.20.0/22
+91.108.36.0/23
+91.108.38.0/23
+91.108.56.0/22
+149.154.160.0/20
+149.154.164.0/22
+204.246.176.0/20
+149.154.172.0/22
+EOF
+}
+
+gfw_dns(){
+    ipset add gfwlist $dns2_ip 2>/dev/null
+}
+
+func_conf(){
+    if grep -q "min-cache-ttl" "$DNSMASQ_RURE"
     then
-        sed -i '/no-resolv/d; /server=127.0.0.1/d' $DNSMASQ_RURE
+        echo ''
+    else
+        cat >> $DNSMASQ_RURE << EOF
+min-cache-ttl=1800
+EOF
     fi
-    cat >> $DNSMASQ_RURE << EOF
+    ipset_init && \
+    gfw_dns && \
+    if [ -f "$local_chnlist_file" ]; then
+        if [ -f "$local_gfwlist_file" ]; then
+            /usr/bin/chinadns-ng -b 0.0.0.0 -l 65353 -c $wan_dns,114.114.114.114 -t $SS_SERVER_LINK#$ss_tunnel_local_port -g $local_gfwlist_file -4 chnroute -M -m $local_chnlist_file >/dev/null 2>&1 &
+	else
+            /usr/bin/chinadns-ng -b 0.0.0.0 -l 65353 -c $wan_dns,114.114.114.114 -t $SS_SERVER_LINK#$ss_tunnel_local_port -4 chnroute -M -m $local_chnlist_file >/dev/null 2>&1 &
+	fi
+    else
+        /usr/bin/chinadns-ng -b 0.0.0.0 -l 65353 -c $wan_dns,114.114.114.114 -t $SS_SERVER_LINK#$ss_tunnel_local_port -4 chnroute >/dev/null 2>&1 &
+    fi
+    if [ $(nvram get sdns_enable) = "1" ]; then
+        if grep -q "no-resolv" "$DNSMASQ_RURE"
+	then
+            echo ''
+        else
+            cat >> $DNSMASQ_RURE << EOF
 no-resolv
 server=127.0.0.1#65353
 EOF
-restart_dhcpd
+        restart_dhcpd
+        fi
+    else
+        if grep -q "65353" "$DNSMASQ_RURE"
+        then
+            sed -i '/no-resolv/d; /server=127.0.0.1/d' $DNSMASQ_RURE
+        fi
+        cat >> $DNSMASQ_RURE << EOF
+no-resolv
+server=127.0.0.1#65353
+EOF
+    restart_dhcpd
+    fi
 }
 
-func_lan_ip(){
+func_gmlan(){
+ip rule add fwmark 0x01/0x01 table 100 2>/dev/null
+ip route add local 0.0.0.0/0 dev lo table 100 2>/dev/null
 ipset -! restore <<-EOF
 create gateway hash:net hashsize 64
 $(gen_lan_ip | sed -e "s/^/add gateway /")
@@ -74,7 +171,7 @@ EOF
 }
 
 gen_lan_ip(){
-cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
+    cat <<-EOF | grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}"
 0.0.0.0/8
 10.0.0.0/8
 100.64.0.0/10
@@ -101,29 +198,31 @@ chmod +x $FWI
 return 0
 }
 
-func_cnng_ipt(){
-if grep -q "vmess" "$STORAGE_V2SH"
-then
-    V2RUL=/tmp/V2mi.txt
-    v2_address=$(sed -n "2p" $V2RUL | cut -f 2 -d ":")
-else
-    v2_address=$(cat $STORAGE_V2SH | grep "address" | awk -F '[:/]' '{print $2}')
-fi
+func_ipt_n(){
 ipt="iptables -t nat"
 
 $ipt -N CNNG_OUT
 $ipt -N CNNG_PRE
 
-$ipt -A PREROUTING -j CNNG_OUT
-$ipt -A OUTPUT -j CNNG_PRE
-$ipt -A CNNG_PRE -d $v2_address -j RETURN
-$ipt -A CNNG_PRE -m set --match-set gateway dst -j RETURN
-$ipt -A CNNG_PRE -m set --match-set chnroute dst -j RETURN
-#$ipt -A CNNG_OUT -m set --match-set chnroute dst -j RETURN
-$ipt -A CNNG_OUT -p udp -d 127.0.0.1 --dport 53 -j REDIRECT --to-ports 65353
+$ipt -I PREROUTING -i br0 -p tcp -j CNNG_OUT
+$ipt -I OUTPUT -p tcp -j CNNG_PRE
 
+$ipt -A CNNG_PRE -d $v2_address -p tcp -m tcp ! --dport 53 -j RETURN
+$ipt -A CNNG_PRE -m set --match-set gateway dst -j RETURN
+$ipt -A CNNG_OUT -m set --match-set chnroute dst -j RETURN
+$ipt -A CNNG_OUT -p udp -d $SS_SERVER_LINK --dport 53 -j REDIRECT --to-ports 65353
 $ipt -A CNNG_OUT -p tcp -j CNNG_PRE
+$ipt -A CNNG_PRE -p tcp -j RETURN -m mark --mark 0xff
 $ipt -A CNNG_PRE -p tcp -j REDIRECT --to-ports 12345
+
+$ipt -A CNNG_OUT -d 0.0.0.0/8 -j RETURN
+$ipt -A CNNG_OUT -d 10.0.0.0/8 -j RETURN
+$ipt -A CNNG_OUT -d 127.0.0.0/8 -j RETURN
+$ipt -A CNNG_OUT -d 172.16.0.0/12 -j RETURN
+$ipt -A CNNG_OUT -d 192.168.0.0/16 -j RETURN
+$ipt -A CNNG_OUT -d 240.0.0.0/4 -j RETURN
+
+$ipt -A CNNG_OUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $ss_local_port
 
 cat <<-CAT >>$FWI
 iptables-save -c | grep -v CNNG_ | iptables-restore -c
@@ -139,14 +238,11 @@ func_start(){
     func_v2txt && \
     func_del_rule && \
     echo -e "\033[41;37m 部署 [CHINADNS-NG] 文件,请稍后...\e[0m\n"
-    #func_cdn_file &
-    wait
-    echo ""
     func_del_ipt
-    func_cnng_file
-    func_lan_ip && \
-    flush_ipt_file && \
-    func_cnng_ipt &
+    func_gmlan && flush_ipt_file && func_ipt_n
+    wait
+    echo "dns"
+    func_conf
     logger -t "[CHINADNS-NG]" "开始运行…"
 }
 
@@ -175,4 +271,3 @@ stop)
     exit 1
     ;;
 esac
-
